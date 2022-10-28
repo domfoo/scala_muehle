409d36f40199a5fe49bd8c7dad69facf567f05ea
Dominik Bartsch <dominik.bartsch@gmx.net>
2022-10-28 08:23:00 +0200

    add file for model tests

---
+++ src/test/scala/model/PlayFieldSpec.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.muehle
+
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.Anywordspec
+
+class PlayFieldSpec extends Anywordspec {
+    "Muehle" should {
+        
+    }
+}
