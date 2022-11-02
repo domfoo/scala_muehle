71d4301c206eabd15d92e08e7509605fd45bec5e
julienco03 <julian160903@gmail.com>
2022-11-02 15:06:46 +0100

    Work on all classes...

--- src/main/scala/de/htwg/se/muehle/aview/TUI.scala
+++ src/main/scala/de/htwg/se/muehle/aview/TUI.scala
@@ -4,6 +4,7 @@ package aview
 import model.Move
 import model.Stone
 import model.Player
+import model.Stones
 import scala.io.StdIn.readLine
 import de.htwg.se.muehle.model.PlayField
 
@@ -11,6 +12,7 @@ import de.htwg.se.muehle.model.PlayField
 class TUI():
 
     val field = new PlayField(3)
+    val mills = new Stones().stones
     val greeting = "-----WELCOME TO MILL-----"
     val playfield_info =
         "This is how the playfield looks like.\n\n" +
@@ -27,8 +29,26 @@ class TUI():
         "|   18----- 19----- 20  |"+ "\n" +
         "|           |           |"+ "\n" +
         "21--------- 22--------- 23"+ "\n\n" +
-        "Type X/O or x/o and two numbers to place a stone.\n" +
+        "Please type set OR move.\n" +
         "Press q to exit.\n"
+    val playfield =
+        "\n%s-----------%s-----------%s\n"+
+        "|           |           |\n"+
+        "|   %s-------%s-------%s   |\n"+
+        "|   |       |       |   |\n"+
+        "|   |   %s---%s---%s   |   |\n"+
+        "|   |   |       |   |   |\n"+
+        "%s---%s---%s       %s---%s---%s\n"+
+        "|   |   |       |   |   |\n"+
+        "|   |   %s---%s---%s   |   |\n"+
+        "|   |       |       |   |\n"+
+        "|   %s-------%s-------%s   |\n"+
+        "|           |           |\n"+
+        "%s-----------%s-----------%s\n"
+    val log = playfield.format(mills(0),mills(1),mills(2),mills(3),mills(4),mills(5),
+                            mills(6),mills(7),mills(8),mills(9),mills(10),mills(11),
+                            mills(12),mills(13),mills(14),mills(15),mills(16),mills(17),
+                            mills(18),mills(19),mills(20),mills(21),mills(22),mills(23))
 
     def run =
         println(greeting)
@@ -39,32 +59,40 @@ class TUI():
         while(true)
             print("> ")
             handleInput(readLine)
-            print(field)
+            print(log)
 
-    def handleInput(input: String): Option[Move] =
+
+    def handleInput(input: String): Move =
         input match
             case "q" =>
                 println("Bye!")
                 System.exit(0)
-                None
-            case _ => {
-                val chars = input.toCharArray
-                val stone =  chars(0) match
-                        case 'x' => Stone.X
-                        case 'X' => Stone.X
-                        case 'o' => Stone.O
-                        case 'O' => Stone.O
-                        case _   => Stone.Empty
-                val x = chars(1).toInt
-                val y = chars(2).toInt
-                Some(Move(stone, x, y))
-            }
+                Move()
+            case "set" =>
+                println("Type a number from 0-23")
+                val newPos = getPosition()
+                Move()
+            case "move" =>
+                println("Type old position (0-23)")
+                val oldPos = getPosition()
+                println("Type new position (0-23)")
+                val newPos = getPosition()
+                Move()
 
-    def update(player: Player, millsCount: Int, gameEnded: Boolean): String = {
+    def getPosition(): Option[Int] = {
+        readLine().toInt match
+            case n: Int if n >= 0 && n <= 23 =>
+                Some(n)
+            case _ =>
+                println("Error: number has to be from 0-23!")
+                None
+    }
+
+    /**def update(player: Player, millsCount: Int, gameEnded: Boolean): String = {
         if (gameEnded)
             player.getName + " has won!"
         else if (millsCount > 0)
-            player.getName + " has " + millsCount + " mills, remove " + millsCount + " stones!"
+            player.getName + " has " + millsCount + " mills, remove " + millsCount + " mills!"
         else
             player.getName + ", it's your turn!"
-    }
+    }*/
--- src/main/scala/de/htwg/se/muehle/model/Player.scala
+++ src/main/scala/de/htwg/se/muehle/model/Player.scala
@@ -1,5 +1,3 @@
 package de.htwg.se.muehle.model
 
-case class Player(name: String, color: Char):
-    def getName = this.name
-    def getColor = this.color
+case class Player(name: String, stones: Int = 9)
