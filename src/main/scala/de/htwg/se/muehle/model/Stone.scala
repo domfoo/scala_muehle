ea003daf844dca338459b6141f4c8cbefa4f112f
julienco03 <julian160903@gmail.com>
2022-11-05 15:38:14 +0100

    Replace spaces with tabs

--- src/main/scala/de/htwg/se/muehle/model/Stone.scala
+++ src/main/scala/de/htwg/se/muehle/model/Stone.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.muehle.model
 
 enum Stone(stoneAsString: String):
-  override def toString = stoneAsString
-  case X extends Stone("X")
-  case O extends Stone("O")
-  case Empty extends Stone("+")
+    override def toString = stoneAsString
+    case X extends Stone("X")
+    case O extends Stone("O")
+    case Empty extends Stone("+")
