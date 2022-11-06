c0020bcf02c5e52255fadc0a4706e5e37bbe854a
julienco03 <julian160903@gmail.com>
2022-11-06 10:21:13 +0100

    Add equals() method

--- src/main/scala/de/htwg/se/muehle/model/Player.scala
+++ src/main/scala/de/htwg/se/muehle/model/Player.scala
@@ -1,7 +1,10 @@
 package de.htwg.se.muehle.model
 
-case class Player(name: String, stones: Int):
-    def this(name: String) = {
-        this(name = name, stones = 9)
-    }
+case class Player(name: String, stones: Int = 9):
     override def toString: String = s"Name: $name, stones left: $stones"
+    override def equals(player: Any): Boolean = {
+        player match {
+            case p:Player => p.name.equals(name) && p.stones.equals(stones)
+            case _ => false
+        }
+    }
