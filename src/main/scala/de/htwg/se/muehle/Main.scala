e4c5e90f81b6d98a9791e2b94383973adab4ea00
julienco03 <julian160903@gmail.com>
2022-11-06 14:08:05 +0100

    Update main

--- src/main/scala/de/htwg/se/muehle/Main.scala
+++ src/main/scala/de/htwg/se/muehle/Main.scala
@@ -1,7 +1,12 @@
 package de.htwg.se.muehle
 
 import aview.TUI
+import controller.Controller
+import model.Matrix
+import model.Stone
 
 @main def run: Unit =
-    val tui = TUI()
+    val field = new Matrix(7, Stone.Empty)
+    val controller = Controller(field)
+    val tui = TUI(controller)
     tui.run
