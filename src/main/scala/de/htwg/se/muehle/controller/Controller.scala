018744bf1008a1ff64fe5f7cf3536fc055e50e67
julienco03 <julian160903@gmail.com>
2022-11-06 14:07:11 +0100

    Implement simple controller

--- src/main/scala/de/htwg/se/muehle/controller/Controller.scala
+++ src/main/scala/de/htwg/se/muehle/controller/Controller.scala
@@ -1,2 +1,15 @@
-package de.htwg.se.muehle.controller
+package de.htwg.se.muehle
+package controller
 
+import model.Matrix
+import model.Player
+import de.htwg.se.muehle.util.Observable
+
+class Controller(var field: Matrix) extends Observable:
+    val players = new Array[Player](2)
+    def addPlayerOne(name: String, stones: Int = 9) = {
+        players(0) = Player(name, stones)
+    }
+    def addPlayerTwo(name: String, stones: Int = 9) = {
+        players(1) = Player(name, stones)
+    }
