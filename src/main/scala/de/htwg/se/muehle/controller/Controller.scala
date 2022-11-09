e162fd242cc575ee607f15540cb5d7c935491b90
Dominik Bartsch <dominik.bartsch@gmx.net>
2022-11-09 10:14:54 +0100

    refactoring the use of stones

--- src/main/scala/de/htwg/se/muehle/controller/Controller.scala
+++ src/main/scala/de/htwg/se/muehle/controller/Controller.scala
@@ -3,13 +3,14 @@ package controller
 
 import model.Matrix
 import model.Player
+import model.Stone
 import de.htwg.se.muehle.util.Observable
 
 class Controller(var field: Matrix) extends Observable:
     val players = new Array[Player](2)
     def addPlayerOne(name: String, stones: Int = 9) = {
-        players(0) = Player(name, stones, "X")
+        players(0) = Player(name, stones, Stone.X)
     }
     def addPlayerTwo(name: String, stones: Int = 9) = {
-        players(1) = Player(name, stones, "O")
+        players(1) = Player(name, stones, Stone.O)
     }
--- src/main/scala/de/htwg/se/muehle/model/Player.scala
+++ src/main/scala/de/htwg/se/muehle/model/Player.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.muehle.model
 
-case class Player(name: String, stones: Int = 9, stoneType: String):
-    override def toString: String = s"Name: $name, stones left: $stones"
+case class Player(name: String, stones: Int = 9, stoneType: Stone):
+    override def toString: String = s"Name: $name, stones left: $stones, stone type: $stoneType"
     override def equals(player: Any): Boolean = {
         player match {
             case p:Player => p.name.equals(name) && p.stones.equals(stones)
