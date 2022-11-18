e760ecfcac6a93513ad2f52a276a52fabb1eff47
Dominik Bartsch <dominik.bartsch@gmx.net>
2022-11-18 09:31:07 +0100

    add tests for player init function in controller

--- src/test/scala/de/htwg/se/muehle/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/muehle/controller/ControllerSpec.scala
@@ -12,8 +12,7 @@ import org.scalatest.matchers.should.Matchers._
 class ControllerSpec extends AnyWordSpec {
     "A Controller" should {
         val controller = Controller(new Field())
-        controller.player1 = Player("Tom", Stone.X)
-        controller.player2 = Player("Max", Stone.O)
+        controller.initPlayers("Tom", "Max")
         "switch from players" in {
             controller.nextPlayer(controller.player1) should be(controller.player2)
             controller.nextPlayer(controller.player2) should be(controller.player1)
