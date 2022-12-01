abfcaba823b5ea05b20a6f8e8db916d44ebc8a43
Dominik Bartsch <dominik.bartsch@gmx.net>
2022-12-02 00:47:07 +0100

    add tests

--- src/test/scala/de/htwg/se/muehle/aview/TUISpec.scala
+++ src/test/scala/de/htwg/se/muehle/aview/TUISpec.scala
@@ -15,15 +15,39 @@ class TUISpec extends AnyWordSpec {
         val controller = Controller(Field())
         val tui = TUI(controller)
         "reading and handling input" should {
+            "recognize the input 'undo' as not undoing anything if there are no prior commands" in {
+                tui.handleInput("undo", Stone.X) should be(Right("undo"))
+                controller.undo()
+            }
+            "recognize the input 'redo' as not redoing anything if there are no next commands" in {
+                tui.handleInput("redo", Stone.X) should be(Right("redo"))
+                controller.redo()
+            }
             "recognize the input 'set 2' as setting the stone X to cell 2" in {
                 val strat = Put(2, Stone.X)
                 tui.handleInput("set 2", Stone.X) should be(Left(strat))
                 controller.executeStrategy(strat)
             }
+            "recognize the input 'undo' as undoing a set command" in {
+                tui.handleInput("undo", Stone.X) should be(Right("undo"))
+                controller.undo()
+            }
+            "recognize the input 'redo' as redoing a set command" in {
+                tui.handleInput("redo", Stone.X) should be(Right("redo"))
+                controller.redo()
+            }
             "recognize the input 'move 2 3' as moving the stone X from cell 2 to cell 3" in {
                 val strat = Move(2, 3, Stone.X)
                 tui.handleInput("move 2 3", Stone.X) should be(Left(strat))
                 controller.executeStrategy(strat)
+            }
+            "recognize the input 'undo' as undoing a move command" in {
+                tui.handleInput("undo", Stone.X) should be(Right("undo"))
+                controller.undo()
+            }
+            "recognize the input 'redo' as redoing a move command" in {
+                tui.handleInput("redo", Stone.X) should be(Right("redo"))
+                controller.redo()
             }
             "recognize invalid input" in {
                 tui.handleInput("hello world", Stone.X) should be(Right("hello"))
