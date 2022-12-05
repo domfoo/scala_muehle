e8ec19ae7d7179346b627ea19a30997e74bd199f
julienco03 <julian160903@gmail.com>
2022-12-05 14:00:54 +0100

    Playfield is now printable on the GUI

--- src/main/scala/de/htwg/se/muehle/aview/SwingGUI.scala
+++ src/main/scala/de/htwg/se/muehle/aview/SwingGUI.scala
@@ -4,22 +4,21 @@ package aview
 import model.Field
 import model.Player
 import model.Stone
-import model.PlayStrategy
+import model.{PlayStrategy, Put, Move}
 import controller.Controller
 import util.Observer
 import scala.swing._
 import scala.swing.event._
 import java.awt.Dimension
 import scala.swing.Swing.EmptyBorder
-
-class CellClicked(val row: Int, val column: Int) extends Event
+import javax.swing.SwingUtilities
+import scala.util.Try
 
 class SwingGUI(controller: Controller) extends Frame with Observer:
   controller.add(this)
   title = "Nine men's morris"
-  preferredSize = new Dimension(600, 600)
-  resizable = false
-  visible = true
+  preferredSize = new Dimension(800, 600)
+  resizable = true
 
   menuBar = new MenuBar {
     contents += new Menu("File") {
@@ -43,46 +42,48 @@ class SwingGUI(controller: Controller) extends Frame with Observer:
     }
   }
 
-  val label = new Label("Welcome to Nine Men's Morris".toUpperCase())
-  val board = new CellPanel()
-
   contents = new BorderPanel {
-    add(label, BorderPanel.Position.North)
-    add(board, BorderPanel.Position.Center)
+    add(new Label("Welcome to Nine Men's Morris".toUpperCase()), BorderPanel.Position.North)
+    add(new CellPanel(), BorderPanel.Position.Center)
   }
-  //reactions += {}
 
   pack()
   centerOnScreen()
   open()
 
-  def update: Unit = repaint()
+  def update: Unit =
+    // TODO: effizientere Lösung finden anstatt jedes mal ein neues Frame zu erzeugen
+    this.dispose()
+    new SwingGUI(controller)
+    visible = true
 
   class CellPanel() extends GridPanel(7, 7):
     border = EmptyBorder(20,20,20,20)
-    val list = fieldToList()
-    // TODO: in jedem button soll jeweils das richtige listenelement stehen
-    (1 to 49).map(x => if (cellIsDefined(x)) contents += new Button(list.toString) else contents += new Button(""))
+    printField
 
-    /*list.map(y =>
-      (1 until 49).map(x =>
-        if (cellIsDefined(x)) contents += new Button(list.toString) else contents += new Button("")
-    ))*/
+    def printField: IndexedSeq[Matchable] =
+      var i = 0
+      val list = fieldToList()
+      (1 to 49).map(x =>
+        if (cellIsDefined(x)) { contents += new CellButton((i+1).toInt, list(i).toString); i += 1}
+        else contents += new Button(""))
 
-    def button(stone: Stone) = new Button(stone.toString)
+    def fieldToList(): List[Stone] =
+      controller.field.cells.values.toList
+    def definedCells: List[Int] =
+      List(1,4,7,9,11,13,17,18,19,22,23,24,26,27,28,31,32,33,37,39,41,43,46,49)
+    def cellIsDefined(pos: Int): Boolean =
+      if (definedCells.contains(pos)) true else false
 
   class CellButton(pos: Int, stone: String) extends Button(stone):
     listenTo(mouse.clicks)
     reactions += {
-      case MouseClicked(src, pt, mod, clicks, props) => {
-        // TODO: case cell ist leer, dann set
-        // TODO: case cell ist besetzt, dann move möglich
-      }
+      case ButtonClicked(button) =>
+        controller.executeStrategy(Put(pos, controller.nextPlayer().stoneType))
+      // TODO: move implementieren
+      /*case ButtonClicked(button) =>
+        case ButtonClicked(button2) =>
+          controller.executeStrategy(Move(, , controller.nextPlayer().stoneType))
+          button.location
+        case _ =>*/
     }
-
-  def definedCells: List[Int] = List(1,4,7,9,11,13,17,18,19,22,23,24,26,27,28,31,32,33,37,39,41,43,46,49)
-  def cellIsDefined(pos: Int): Boolean =
-        if (definedCells.contains(pos)) true else false
-
-  def fieldToList(): List[Stone] =
-        controller.field.cells.values.toList
