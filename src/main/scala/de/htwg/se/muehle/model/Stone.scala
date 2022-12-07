28fefdc985782077aa6f0a6a17e359442100f820
julienco03 <julian160903@gmail.com>
2022-12-07 08:54:24 +0100

    Add toStone() method

--- src/main/scala/de/htwg/se/muehle/model/Stone.scala
+++ src/main/scala/de/htwg/se/muehle/model/Stone.scala
@@ -2,6 +2,11 @@ package de.htwg.se.muehle.model
 
 enum Stone(stoneAsString: String):
     override def toString = stoneAsString
+    def toStone(stoneAsString: String) = stoneAsString match {
+        case "X" => Stone.X
+        case "O" => Stone.O
+        case "+" => Stone.Empty
+    }
     case X extends Stone("X")
     case O extends Stone("O")
     case Empty extends Stone("+")
