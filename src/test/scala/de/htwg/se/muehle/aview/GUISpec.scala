713f9b50edd06e6eab130e77d1e58e9baa99620f
julienco03 <julian160903@gmail.com>
2022-12-07 19:08:56 +0100

    Add tests for GUI

---
+++ src/test/scala/de/htwg/se/muehle/aview/GUISpec.scala
@@ -0,0 +1,28 @@
+package de.htwg.se.muehle
+package aview
+
+import aview.GUI
+import controller.Controller
+import model.Field
+import model.{PlayStrategy, Put, Move}
+import model.Player
+import model.Stone
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class GUISpec extends AnyWordSpec {
+    
+    "The GUI" should {
+        val controller = Controller(Field())
+        val gui = GUI(controller)
+        "create a frame in which you can" should {
+            "set a stone" in { }
+            "move a stone" in { }
+            "convert a string to a stone" in {
+                gui.toStone("X") should be(Stone.X)
+                gui.toStone("O") should be(Stone.O)
+                gui.toStone("+") should be(Stone.Empty)
+            }
+        }
+    }
+}
