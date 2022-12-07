5f07e560e2caef2bcdf8e0aaac590bcb71cd1ea6
julienco03 <julian160903@gmail.com>
2022-12-07 15:14:00 +0100

    Remove a few imports

--- src/test/scala/de/htwg/se/muehle/aview/GUISpec.scala
+++ src/test/scala/de/htwg/se/muehle/aview/GUISpec.scala
@@ -2,7 +2,6 @@ package de.htwg.se.muehle
 package aview
 
 import aview.GUI
-import aview.GUI.CellButton
 import controller.Controller
 import model.Field
 import model.{PlayStrategy, Put, Move}
@@ -10,8 +9,6 @@ import model.Player
 import model.Stone
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
-import scala.swing._
-import scala.swing.event._
 
 class GUISpec extends AnyWordSpec {
     "The GUI" should {
