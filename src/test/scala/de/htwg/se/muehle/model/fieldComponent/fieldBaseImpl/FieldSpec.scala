7dd5cf88f6840b6c5e61ddf97834f03f6dc2def6
julienco03 <julian160903@gmail.com>
2022-12-15 23:15:37 +0100

    Work on mill detection

--- src/test/scala/de/htwg/se/muehle/model/fieldComponent/fieldBaseImpl/FieldSpec.scala
+++ src/test/scala/de/htwg/se/muehle/model/fieldComponent/fieldBaseImpl/FieldSpec.scala
@@ -1,6 +1,7 @@
 package de.htwg.se.muehle.model
 
 import de.htwg.se.muehle.model.fieldComponent.fieldBaseImpl.Field
+import de.htwg.se.muehle.model.fieldComponent.fieldBaseImpl.Stone
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
@@ -10,6 +11,14 @@ class FieldSpec extends AnyWordSpec {
         val field0 = Field(0)
         "have an empty string for field size 0" in {
             field0.toString should be("" + field0.eol)
+        }
+        "have a row or column filled with stones of a specific type" in {
+            field0.replaceCell(1, Stone.X)
+            field0.replaceCell(2, Stone.X)
+            field0.replaceCell(3, Stone.X)
+            field0.isFullMill(3, Stone.X) should be(true)
+            field0.isFullMill(3, Stone.O) should be(false)
+            field0.isFullMill(4, Stone.X) should be(false)
         }
     }
 }
